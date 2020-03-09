1af174c12293f376f916dcfd8b8c8aa0250b6331
JarbiBonilla <jarbibonilla@gmail.com>
2020-03-09 19:11:52 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :clickbait
+
+    def clickbait
+        arr = ["Won't Believe", "Secret", "Guess"]
+        if title 
+            if !arr.any? {|a| title.include?(a)}
+                errors.add(:title, "Must Include Clickbait")
+            end
+        end
+    end
 end
+
+# If the title does not contain "Won't Believe",
+# "Secret", "Top [number]", or "Guess", the validator should add a validation.
