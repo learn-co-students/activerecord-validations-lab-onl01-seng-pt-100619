653bc4c3816f19c63833c9cfc77fad13563da3c4
Phil Billingsby <philbillingsby@gmail.com>
2020-03-10 11:10:56 -0400

    Done.

--- app/validators/post_validator.rb
+++ app/validators/post_validator.rb
@@ -1,6 +1,6 @@
 class PostValidator < ActiveModel::Validator
   def validate(record)
-    if record.title && !record.title.match(/Won't Believe| Not [\d*]|Secret| Guess/)
+    if record.title && !record.title.match(/Won't Believe| Top [\d*]|Secret| Guess/)
       record.errors.add(:title, "Not click bait enough")
     end
   end
