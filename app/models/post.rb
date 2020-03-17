ccbe17b2e771eb2ee3443c211bf669766c786368
Mel <melchrstmn@gmail.com>
2020-03-17 18:33:42 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Non-Fiction Fiction) }
+    validate :clickbait
+  
+    def clickbait
+      if !title.nil? && (!title.include?( "Won't Believe" || "Secret" || "Top [number]" || "Guess"))
+        errors.add(:title, "Category is not included in the list")
+      end
+    end
 end
